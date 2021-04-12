cdfe5dc24045831a3c5513560a1329ebd505f89b
Alfred Sporre <alfred@familjensporre.se>
2021-04-12 11:46:34 +0200

    feat: shader program registry
    also:
    * add resource folder
    * add test shaders into resource folder

--- src/registries.cpp
+++ src/registries.cpp
@@ -2,7 +2,16 @@
 
 /* -- Register Shader Programs -- */
 
-//void initShaderPrograms(Registry<ShaderProgram> *reg) {}
+void initShaderPrograms(Registry<ShaderProgram> *reg) {
+    Shader testVert = Shader(GL_VERTEX_SHADER, "./res/shader/test.vert");
+    Shader testFrag = Shader(GL_FRAGMENT_SHADER, "./res/shader/test.frag");
+    reg->put("test", new ShaderProgram({&testVert, &testFrag}));
+}
+
+/* -- Initialization Function -- */
+void REG::init() {
+    _shaderPrograms.init();
+}
 
 /* -- Registry Instantiations -- */
-//Registry<ShaderProgram> const REG::SHADER_PROGRAMS(initShaderPrograms);
+Registry<ShaderProgram> REG::_shaderPrograms(initShaderPrograms);
