e950cc91c9ba6172c2355e4f34d364d6f7649fe3
Alfred Sporre <alfred@familjensporre.se>
2021-04-13 10:46:49 +0200

    feat: texture class

---
+++ src/texture.cpp
@@ -0,0 +1,42 @@
+#include "texture.h"
+
+Texture::Texture() {
+    // Generate texture
+    glGenTextures(1, &_handle);
+}
+
+Texture::Texture(std::string const& path) : Texture() {
+    _loadTexture(path);
+}
+
+Texture* Texture::bind(uint32_t t) {
+    glActiveTexture(GL_TEXTURE0 + t);
+    glBindTexture(GL_TEXTURE_2D, _handle);
+    return this;
+}
+
+Texture* Texture::setMinMagFilter(GLenum val) {
+    bind();
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, val);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, val);
+    return this;
+}
+Texture* Texture::setWrap(GLenum val) {
+    bind();
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, val);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, val);
+    return this;
+}
+
+void Texture::_loadTexture(std::string const& path) {
+    // Load texture data
+    unsigned char* data = FileLoader::loadTexture(path, &_width, &_height, &_numChannels);
+
+    // Send to GPU
+    bind();
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, _width, _height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
+}
+
+Texture::~Texture() {
+    glDeleteTextures(1, &_handle);
+}
---
+++ src/texture.h
@@ -0,0 +1,36 @@
+#pragma once
+
+#include <glad/glad.h>
+
+#include <string>
+
+#include "fileloader.h"
+
+class Texture {
+   public:
+    Texture();
+    Texture(std::string const& path);
+    ~Texture();
+
+    // Binds this texture to the specified texture slot
+    Texture* bind(uint32_t t = 0);
+
+    /* - Parameter Setters - */
+
+    Texture* setMinMagFilter(GLenum val);
+    Texture* setWrap(GLenum val);
+
+    /* - Parameter Getters - */
+
+    inline uint32_t getWidth() { return _width; }
+    inline uint32_t getHeight() { return _height; }
+    inline uint32_t getNumChannels() { return _numChannels; }
+
+    inline GLuint getHandle() { return _handle; }
+
+   private:
+    uint32_t _width{0}, _height{0}, _numChannels{0};
+    GLuint _handle{0};
+
+    void _loadTexture(std::string const& path);
+};
