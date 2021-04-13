e8cc20ba7315946c80f6de959954768571081418
Alfred Sporre <alfred@familjensporre.se>
2021-04-13 10:47:54 +0200

    feat: texture registry
    also:
    * add default texture to resource folder
    * load default texture into registry

--- src/registries.cpp
+++ src/registries.cpp
@@ -8,10 +8,19 @@ void initShaderPrograms(Registry<ShaderProgram> *reg) {
     reg->put("test", new ShaderProgram({&testVert, &testFrag}));
 }
 
+/* -- Register Textures -- */
+void initTextures(Registry<Texture> *reg) {
+    reg->put("default", (new Texture("./res/texture/default.png"))
+                            ->setMinMagFilter(GL_NEAREST)
+                            ->setWrap(GL_REPEAT));
+}
+
 /* -- Initialization Function -- */
 void REG::init() {
     _shaderPrograms.init();
+    _textures.init();
 }
 
 /* -- Registry Instantiations -- */
 Registry<ShaderProgram> REG::_shaderPrograms(initShaderPrograms);
+Registry<Texture> REG::_textures(initTextures);
--- src/registries.h
+++ src/registries.h
@@ -3,13 +3,16 @@
 
 #include "registry.h"
 #include "shaderprogram.h"
+#include "texture.h"
 
 /* -- Registry Declarations -- */
 class REG {
    public:
     static void init();
     static inline const Registry<ShaderProgram> SHADER_PROGRAMS() { return _shaderPrograms; }
+    static inline const Registry<Texture> TEXTURES() { return _textures; }
 
    private:
     static Registry<ShaderProgram> _shaderPrograms;
+    static Registry<Texture> _textures;
 };
