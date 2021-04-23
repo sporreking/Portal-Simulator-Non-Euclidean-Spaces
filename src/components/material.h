ef1fb64d8414b484899a85a68143efef14d5ba46
Mattias Beming <mattias.beming@outlook.com>
2021-04-23 11:54:23 +0200

    fix: add const& to color parameter in material constructor

--- src/components/material.h
+++ src/components/material.h
@@ -12,7 +12,7 @@ class Material : public Component {
     Material(Texture *tex) : Material(tex, 1.0, 0.0, 0.0) {}
     Material(Texture *tex, double kd, double ks, double alpha)
         : Material(tex, kd, ks, alpha, {1.0, 1.0, 1.0}) {}
-    Material(Texture *tex, double kd, double ks, double alpha, glm::vec3 color)
+    Material(Texture *tex, double kd, double ks, double alpha, glm::vec3 const &color)
         : TEXTURE{tex}, kd{kd}, ks{ks}, alpha{alpha}, color{color} {}
 
     void update(double const &dt) override {}
