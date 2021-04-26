98e4820db3df2f7b75bbb794d68a1497f55758bd
Alfred Sporre <alfred@familjensporre.se>
2021-04-26 10:54:09 +0200

    feat: range support for point lights

--- src/components/pointlight.h
+++ src/components/pointlight.h
@@ -6,8 +6,8 @@
 namespace COMP {
 class PointLight : public Component {
    public:
-    PointLight(glm::vec3 const& color)
-        : color{color} {}
+    PointLight(glm::vec3 const& color, double range = 5.0)
+        : color{color}, range{range} {}
 
     void update(double const& dt) override {}
     void render(glm::mat4 const& m) override {}
@@ -22,6 +22,11 @@ class PointLight : public Component {
         oldRoom->_removeLight(this);
     }
 
+    inline glm::vec3 position() {
+        return glm::vec3(_parent->getGlobalTransformMatrix() * glm::vec4{0.0, 0.0, 0.0, 1.0});
+    }
+
     glm::vec3 color;
+    double range;
 };
 }  // namespace COMP
