fda11f5bc00ec6ac46ad610029d0bd48053c7e2b
Alfred Sporre <alfred@familjensporre.se>
2021-04-26 10:55:26 +0200

    feat: light sending support for shader programs
    also:
    * update test shaders to check if light sending was correct

--- res/shader/test.vert
+++ res/shader/test.vert
@@ -1,11 +1,23 @@
 #version 430
 
+// Uniform matrices
+layout(location=0) uniform mat4 modelViewMat;
+layout(location=1) uniform mat4 projectionMat;
+
+// Uniform lighting
+layout(location=1000) uniform uint nrLights;
+layout(location=1032) uniform uint lightTypes[32];
+layout(location=1064) uniform vec3 lightPositions[32];
+layout(location=1096) uniform vec3 lightColors[32];
+layout(location=1128) uniform double lightRanges[32];
+
+// Attributes
 layout(location=0) in vec3 in_Position;
-in vec3 in_Color;
+layout(location=1) in vec3 in_Normal;
+layout(location=2) in vec3 in_TexCoords;
 
-out vec3 pass_Color;
+out vec4 pass_Color;
 void main() {
-    gl_Position = vec4(in_Position, 1.0);
-
-    pass_Color = in_Color;
+    gl_Position = vec4(in_Position + lightPositions[0], 1.0);
+    pass_Color = vec4(lightColors[0], lightRanges[0]);
 }
--- src/shaderprogram.h
+++ src/shaderprogram.h
@@ -2,6 +2,7 @@
 
 #include <vector>
 
+#include "components/pointlight.h"
 #include "shader.h"
 
 // Attribute locations
@@ -10,10 +11,30 @@
 #define ATTLOC_TEX_COORDS 2
 #define ATTLOC_COLOR 3
 
+// Uniform locations
+#define UNILOC_MODEL_VIEW_MAT 0
+#define UNILOC_PROJECTION_MAT 1
+
+#define UNILOC_NR_LIGHTS 1000  // Increments according to MAX_LIGHTS
+#define UNILOC_LIGHT_TYPES 1032
+#define UNILOC_LIGHT_POSITIONS 1064
+#define UNILOC_LIGHT_COLORS 1096
+#define UNILOC_LIGHT_RANGES 1128
+
+// Light types
+#define LIGHT_TYPE_POINT 0
+#define LIGHT_TYPE_DIRECTIONAL 1
+
+// Constants
+#define MAX_LIGHTS 32
+
 class ShaderProgram {
    public:
     ShaderProgram(std::vector<Shader*> const& shaders);
     ~ShaderProgram();
+
+    ShaderProgram* sendLights(std::vector<Component*> const& lights,
+                              glm::mat4 const& transformMatrix = glm::mat4{1.0});
 
     inline ShaderProgram* bind() {
         glUseProgram(_handle);
@@ -25,4 +46,16 @@ class ShaderProgram {
    private:
     void _linkShaders(std::vector<Shader*> const& shaders);
     GLuint _handle{0};
+
+    /* -- Lighting -- */
+
+    // General
+
+    uint32_t _lightTypes[MAX_LIGHTS];
+    glm::vec3 _lightPositions[MAX_LIGHTS];
+    glm::vec3 _lightColors[MAX_LIGHTS];
+
+    // Point lights
+
+    double _lightRanges[MAX_LIGHTS];
 };
