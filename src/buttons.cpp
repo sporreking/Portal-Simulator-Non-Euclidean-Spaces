3831b08ca368b3aa631102f1dd6b7d1c808f048b
Mattias Beming <mattias.beming@outlook.com>
2021-04-30 11:20:12 +0200

    feat: ascend axis and mouse toggle button

--- src/buttons.cpp
+++ src/buttons.cpp
@@ -5,19 +5,23 @@ void bindInput() {
     /* BUTTONS */
     Input::bindKeyToButton(BUTTON_JUMP, GLFW_KEY_SPACE);
     Input::bindMouseToButton(BUTTON_FIRE, GLFW_MOUSE_BUTTON_1);
+    Input::bindKeyToButton(BUTTON_TOGGLE_MOUSE, GLFW_KEY_LEFT_ALT);
 
     /* AXIS */
     Input::initAxis(AXIS_LOOK_VERTICAL, 0);
     Input::bindKeysToAxis(AXIS_LOOK_VERTICAL, GLFW_KEY_I, GLFW_KEY_K, 0.5);
-    Input::bindMouseYToAxis(AXIS_LOOK_VERTICAL, 0.1, false);
+    Input::bindMouseYToAxis(AXIS_LOOK_VERTICAL, 0.002, false);
 
     Input::initAxis(AXIS_LOOK_HORIZONTAL, 0);
     Input::bindKeysToAxis(AXIS_LOOK_HORIZONTAL, GLFW_KEY_L, GLFW_KEY_J, 0.5);
-    Input::bindMouseXToAxis(AXIS_LOOK_HORIZONTAL, 0.1, false);
+    Input::bindMouseXToAxis(AXIS_LOOK_HORIZONTAL, 0.002, false);
 
     Input::initAxis(AXIS_FORWARD, 1.0);
     Input::bindKeysToAxis(AXIS_FORWARD, GLFW_KEY_W, GLFW_KEY_S);
 
     Input::initAxis(AXIS_STRAFE, 1.0);
     Input::bindKeysToAxis(AXIS_STRAFE, GLFW_KEY_D, GLFW_KEY_A);
+
+    Input::initAxis(AXIS_ASCEND, 1.0);
+    Input::bindKeysToAxis(AXIS_ASCEND, GLFW_KEY_SPACE, GLFW_KEY_LEFT_SHIFT);
 }
--- src/buttons.h
+++ src/buttons.h
@@ -4,9 +4,11 @@
 /* Register Buttons */
 #define BUTTON_JUMP 1
 #define BUTTON_FIRE 2
+#define BUTTON_TOGGLE_MOUSE 3
 
 /* Register Buttons */
 #define AXIS_LOOK_VERTICAL 101
 #define AXIS_LOOK_HORIZONTAL 102
 #define AXIS_FORWARD 103
 #define AXIS_STRAFE 104
+#define AXIS_ASCEND 105
