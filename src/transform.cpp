789ee66d09dc1186079900195f5dbf78d67eb8cf
Mattias Beming <mattias.beming@outlook.com>
2021-04-30 11:26:39 +0200

    fix: matrix construction order in transform conversion

--- src/transform.cpp
+++ src/transform.cpp
@@ -2,8 +2,9 @@
 
 glm::mat4 Transform::matrix() {
     glm::mat4 m{glm::scale(glm::mat4(1.0), scale)};
+    m = glm::translate(m, pos);
     m = glm::rotate(m, rot.y, glm::vec3(0, 1, 0));
     m = glm::rotate(m, rot.x, glm::vec3(1, 0, 0));
     m = glm::rotate(m, rot.z, glm::vec3(0, 0, 1));
-    return glm::translate(m, pos);
+    return m;
 }
