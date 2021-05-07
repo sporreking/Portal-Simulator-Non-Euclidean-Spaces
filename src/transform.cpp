9ac2da6c1ee5d135e72e7cdf16055ff3df447ea5
Alfred Sporre <alfred@familjensporre.se>
2021-05-07 11:11:05 +0200

    fix: transformation order and specular clamping

--- src/transform.cpp
+++ src/transform.cpp
@@ -1,10 +1,9 @@
 #include "transform.h"
 
 glm::mat4 Transform::matrix() {
-    glm::mat4 m{glm::scale(glm::mat4(1.0), scale)};
-    m = glm::translate(m, pos);
+    glm::mat4 m = glm::translate(glm::mat4(1.0), pos);
     m = glm::rotate(m, rot.y, glm::vec3(0, 1, 0));
     m = glm::rotate(m, rot.x, glm::vec3(1, 0, 0));
     m = glm::rotate(m, rot.z, glm::vec3(0, 0, 1));
-    return m;
+    return glm::scale(m, scale);
 }
