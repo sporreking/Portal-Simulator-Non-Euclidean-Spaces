6d74affe8d728db698bde60b4be2b919bb1536fd
Mattias Beming <mattias.beming@outlook.com>
2021-05-14 09:34:40 +0200

    feat: depth texture support for frame buffers
    also:
    * allow color buffer to be disabled in frame buffers
    * rename isEntryLink to _isEntryLink

--- src/framebuffer.cpp
+++ src/framebuffer.cpp
@@ -1,14 +1,20 @@
 #include "framebuffer.h"
 
-FrameBuffer::FrameBuffer(uint32_t width, uint32_t height, bool useDepthBuffer)
-    : _width{width}, _height{height}, _useDepthBuffer{useDepthBuffer} {
+FrameBuffer::FrameBuffer(uint32_t width, uint32_t height, bool useColorBuffer, DepthBufferConfig c)
+    : _width{width}, _height{height}, _useColorBuffer{useColorBuffer}, _depthBufferConfig{c} {
     _createFrameBuffer();
 }
 
 FrameBuffer::~FrameBuffer() {
     glDeleteFramebuffers(1, &_handle);
-    glDeleteRenderbuffers(1, &_depthBuffer);
-    delete _texture;
+
+    if (_depthBufferConfig == DepthBufferConfig::WRITE_ONLY)
+        glDeleteRenderbuffers(1, &_depthBuffer);
+    else if (_depthBufferConfig == DepthBufferConfig::READ_WRITE)
+        delete _depthTexture;
+
+    if (_useColorBuffer)
+        delete _texture;
 }
 
 FrameBuffer *FrameBuffer::bind() {
@@ -23,14 +29,21 @@ void FrameBuffer::bindDefault() {
 }
 
 void FrameBuffer::init() {
-    for (size_t i{0}; i < NR_FRAME_BUFFERS; i++)
-        _frameBuffers.push_back(new FrameBuffer(WINDOW_WIDTH, WINDOW_HEIGHT, true));
+    for (size_t i{0}; i < NR_COLOR_BUFFERS; i++)
+        _colorBuffers.push_back(new FrameBuffer(WINDOW_WIDTH, WINDOW_HEIGHT, true, DepthBufferConfig::WRITE_ONLY));
+
+    for (size_t i{0}; i < NR_DEPTH_BUFFERS; i++)
+        _depthBuffers.push_back(new FrameBuffer(WINDOW_WIDTH, WINDOW_HEIGHT, false, DepthBufferConfig::READ_WRITE));
 }
 
 void FrameBuffer::free() {
-    for (size_t i{0}; i < NR_FRAME_BUFFERS; i++)
-        delete _frameBuffers[i];
-    _frameBuffers.clear();
+    for (size_t i{0}; i < NR_COLOR_BUFFERS; i++)
+        delete _colorBuffers[i];
+    _colorBuffers.clear();
+
+    for (size_t i{0}; i < NR_DEPTH_BUFFERS; i++)
+        delete _depthBuffers[i];
+    _depthBuffers.clear();
 }
 
 void FrameBuffer::_createFrameBuffer() {
@@ -38,27 +51,37 @@ void FrameBuffer::_createFrameBuffer() {
     glGenFramebuffers(1, &_handle);
     glBindFramebuffer(GL_FRAMEBUFFER, _handle);
 
-    // Allocate texture memory
-    _texture = new Texture;
-    _texture->bind();
-    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, _width, _height, 0,
-                 GL_RGB, GL_UNSIGNED_BYTE, 0);
-    _texture->setMinMagFilter(GL_NEAREST);
+    if (_useColorBuffer) {
+        // Allocate texture memory
+        _texture = new Texture;
+        _texture->bind();
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, _width, _height, 0,
+                     GL_RGB, GL_UNSIGNED_BYTE, 0);
+        _texture->setMinMagFilter(GL_NEAREST);
+
+        // Bind texture to frame buffer
+        glFramebufferTexture(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, _texture->getHandle(), 0);
+
+        // Map shader output to texture
+        GLenum drawBuffers[1] = {GL_COLOR_ATTACHMENT0};
+        glDrawBuffers(1, drawBuffers);
+    }
 
     // Generate and bind depth buffer if applicable
-    if (_useDepthBuffer) {
+    if (_depthBufferConfig == DepthBufferConfig::WRITE_ONLY) {
         glGenRenderbuffers(1, &_depthBuffer);
         glBindRenderbuffer(GL_RENDERBUFFER, _depthBuffer);
         glRenderbufferStorage(GL_RENDERBUFFER, GL_DEPTH_COMPONENT, _width, _height);
         glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, GL_RENDERBUFFER, _depthBuffer);
+    } else if (_depthBufferConfig == DepthBufferConfig::READ_WRITE) {
+        // Allocate depth texture memory
+        _depthTexture = new Texture;
+        _depthTexture->bind();
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_DEPTH_COMPONENT24, _width, _height, 0,
+                     GL_DEPTH_COMPONENT, GL_FLOAT, 0);
+        _depthTexture->setMinMagFilter(GL_NEAREST);
+        glFramebufferTexture(GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, _depthTexture->getHandle(), 0);
     }
-
-    // Bind texture to frame buffer
-    glFramebufferTexture(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, _texture->getHandle(), 0);
-
-    // Map shader output to texture
-    GLenum drawBuffers[1] = {GL_COLOR_ATTACHMENT0};
-    glDrawBuffers(1, drawBuffers);
 
     // Validity check
     if (glCheckFramebufferStatus(GL_FRAMEBUFFER) != GL_FRAMEBUFFER_COMPLETE) {
@@ -70,4 +93,5 @@ void FrameBuffer::_createFrameBuffer() {
     bindDefault();
 }
 
-std::vector<FrameBuffer *> FrameBuffer::_frameBuffers;
+std::vector<FrameBuffer *> FrameBuffer::_colorBuffers;
+std::vector<FrameBuffer *> FrameBuffer::_depthBuffers;
--- src/framebuffer.h
+++ src/framebuffer.h
@@ -5,25 +5,34 @@
 #include "config.h"
 #include "texture.h"
 
+enum DepthBufferConfig {
+    NONE = 0,
+    WRITE_ONLY = 1,
+    READ_WRITE = 2
+};
+
 class FrameBuffer {
    public:
-    FrameBuffer(uint32_t width, uint32_t height, bool useDepthBuffer);
+    FrameBuffer(uint32_t width, uint32_t height, bool useColorBuffer, DepthBufferConfig c);
     ~FrameBuffer();
 
     FrameBuffer *bind();
 
     inline uint32_t getWidth() { return _width; }
     inline uint32_t getHeight() { return _height; }
-    inline bool hasDepthBuffer() { return _useDepthBuffer; }
+    inline bool hasDepthBuffer() { return _depthBufferConfig != NONE; }
+    inline DepthBufferConfig getDepthBufferConfig() { return _depthBufferConfig; }
 
     inline Texture *texture() { return _texture; }
+    inline Texture *depthTexture() { return _depthTexture; }
 
     inline GLuint getHandle() { return _handle; }
 
     /* -- Static -- */
 
     static void bindDefault();
-    static inline FrameBuffer *get(size_t id) { return _frameBuffers.at(id); }
+    static inline FrameBuffer *getColorBuffer(size_t id) { return _colorBuffers.at(id); }
+    static inline FrameBuffer *getDepthBuffer(size_t id) { return _depthBuffers.at(id); }
     static void init();
     static void free();
 
@@ -31,17 +40,20 @@ class FrameBuffer {
     /* -- Parameters -- */
 
     uint32_t _width, _height;
-    bool _useDepthBuffer;
+    bool _useColorBuffer;
+    DepthBufferConfig _depthBufferConfig;
 
     /* -- Core -- */
 
     GLuint _handle{0};
-    GLuint _depthBuffer{0};
     Texture *_texture;
+    GLuint _depthBuffer{0};
+    Texture *_depthTexture;
 
     void _createFrameBuffer();
 
     /* -- Static -- */
 
-    static std::vector<FrameBuffer *> _frameBuffers;
+    static std::vector<FrameBuffer *> _colorBuffers;
+    static std::vector<FrameBuffer *> _depthBuffers;
 };
