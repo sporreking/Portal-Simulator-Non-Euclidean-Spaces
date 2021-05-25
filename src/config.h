05748137507607fb4b62b5786376320871aa8980
Mattias Beming <mattias.beming@outlook.com>
2021-05-25 14:58:43 +0200

    feat: new world files
    also:
    * update window size
    * increase view for far plane
    * fix resize.world

--- src/config.h
+++ src/config.h
@@ -2,8 +2,8 @@
 
 #include "color.h"
 
-#define WINDOW_WIDTH 1280
-#define WINDOW_HEIGHT 720
+#define WINDOW_WIDTH 1920
+#define WINDOW_HEIGHT 1080
 #define WINDOW_TITLE "Non-Euclidean Spaces: Portal Simulator"
 #define BACKGROUND_COLOR \
     { .0, .7, 1.0 }
@@ -11,9 +11,9 @@
 #define FOV 70
 #define ASPECT_RATIO ((double)WINDOW_WIDTH / WINDOW_HEIGHT)
 #define NEAR_PLANE 0.01
-#define FAR_PLANE 100
+#define FAR_PLANE 10000
 
-#define LINK_MAX_RECURSION_DEPTH 2
+#define LINK_MAX_RECURSION_DEPTH 5
 #define LINK_MAX_RECURSION_DEPTH_COLOR COLOR_BLACK
 
 // Number of static frame buffers
